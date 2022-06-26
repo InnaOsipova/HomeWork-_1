a30e7474ca8c9b10130b89024423095e6dac2f8f
Inna Osipova <rutheva_inna@mail.ru>
2022-06-26 14:11:11 +0300

    Наибольшее и наименьшее из двух чисел

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.Write("Ведите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2) 
+{
+    Console.Write("Наибольшее число" );
+    Console.WriteLine(number1);
+    Console.Write("Наименьшее число" );
+    Console.WriteLine(number2);
+}
+else
+{
+    Console.Write("Наибольшее число" );
+    Console.WriteLine(number2);
+    Console.Write("Наименьшее число" );
+    Console.WriteLine(number1);
+}
